c5c79f12b1885417e44d59629995738336f23fa2
Aylin Marie <hello@aylinmarie.co>
2025-03-09 22:39:51 -0400

    Init Button

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
-import { Button } from './Button';
+import Button from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
@@ -26,27 +26,14 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
+    variant: 'primary',
     label: 'Button',
   },
 };
 
 export const Secondary: Story = {
   args: {
-    label: 'Button',
-  },
-};
-
-export const Large: Story = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
+    variant: 'secondary',
     label: 'Button',
   },
 };
--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,39 +1,22 @@
 import React from "react";
+import "./Button.css";
 
-import "./button.css";
-
-export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean;
-  /** What background color to use */
-  backgroundColor?: string;
-  /** How large should the button be? */
-  size?: "small" | "medium" | "large";
-  /** Button contents */
+interface ButtonProps {
   label: string;
-  /** Optional click handler */
   onClick?: () => void;
+  variant?: "primary" | "secondary";
 }
 
-/** Primary UI component for user interaction */
-export const Button = ({
-  primary = false,
-  size = "medium",
+const Button: React.FC<ButtonProps> = ({
   label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+  onClick,
+  variant = "primary",
+}) => {
   return (
-    <button
-      type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
-      {...props}
-    >
+    <button className={`button ${variant}`} onClick={onClick}>
       {label}
     </button>
   );
 };
+
+export default Button;
