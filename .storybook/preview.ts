1b67d3565d61be74c5be0cf5b3bb6f157ca33b72
Aylin Marie <hello@aylinmarie.co>
2025-03-10 13:05:23 -0400

    Add a11y add on

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -8,13 +8,22 @@ const preview: Preview = {
     docs: {
       toc: true,
     },
+
     layout: 'centered',
+
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo'
+    }
   },
   tags: ['autodocs'],
 };
