5c6a018763f74416cdc80bef2619330b955ae6ae
Aylin Marie <hello@aylinmarie.co>
2025-05-31 15:23:46 -0400

    Stop tracking storybook-static folder

--- storybook-static/assets/Header.stories-xbG7qmZf.js
+++
@@ -1,7 +0,0 @@
-import{f as o}from"./index-CI0l_B2Z.js";import{H as a}from"./Header-CMRveO5x.js";import"./Button-D6uvvepx.js";import"./jsx-runtime-BaRI-H7J.js";import"./index-QNseqSXQ.js";const p={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};const d=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,d as __namedExportsOrder,p as default};
--- storybook-static/assets/index-BY3rei3A.js
+++
@@ -1,1 +0,0 @@
-import{r,g as t}from"./index-QNseqSXQ.js";var a=r();const o=t(a);export{o as R,a as r};
